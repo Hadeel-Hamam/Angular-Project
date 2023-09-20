59806265b59ed955af210d61e223d8630bb14b8e
Hadeel-Hamam <hadeel.hamam.sadek@gmail.com>
2023-09-20 16:16:49 +0300

    Upload Edits

--- src/app/Components/product-details/product-details.component.ts
+++ src/app/Components/product-details/product-details.component.ts
@@ -10,43 +10,50 @@ import { ProductsService } from 'src/app/Services/products.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  currentProductId:number=0;
+  currentProductId: number = 0;
   product: IProduct | undefined = undefined;
-  productIDs:number[]=[];
-  constructor(public productService: ProductsService,public activatedRoute:ActivatedRoute,public location:Location,private router:Router) {}
+  productIDs: number[] = [];
+  constructor(
+    public productService: ProductsService,
+    public activatedRoute: ActivatedRoute,
+    public location: Location,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    this.currentProductId=(this.activatedRoute.snapshot.paramMap.get('id'))?Number(this.activatedRoute.snapshot.paramMap.get('id')):0;
-    this.product=this.productService.getProductByID(this.currentProductId);
-    // console.log(this.product);
-    this.productIDs=this.productService.getProductsIDs();
+    this.productIDs = this.productService.getProductsIDs();
+    // this.currentProductId=(this.activatedRoute.snapshot.paramMap.get('id'))?Number(this.activatedRoute.snapshot.paramMap.get('id')):0;
+    // this.product=this.productService.getProductByID(this.currentProductId);
 
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      this.currentProductId = Number(paramMap.get('id'));
+      this.product = this.productService.getProductByID(this.currentProductId);
+    });
+    // console.log(this.product);
   }
-  goBack(){
+  goBack() {
     // window.history.back();
     this.location.back();
   }
-  prevPrd(){
-    let currentIndex=this.productIDs.findIndex((ele)=>ele==this.currentProductId);
+  prevPrd() {
+    let currentIndex = this.productIDs.findIndex(
+      (ele) => ele == this.currentProductId
+    );
     // console.log(currentIndex);
     let prevPrdID;
-    if(currentIndex>0)
-    {
-      prevPrdID=this.productIDs[currentIndex-1];
-      this.router.navigate(['/products',prevPrdID]);
-
+    if (currentIndex > 0) {
+      prevPrdID = this.productIDs[currentIndex - 1];
+      this.router.navigate(['/products', prevPrdID]);
     }
-
   }
-  nextPrd(){
-    let currentIndex=this.productIDs.findIndex((ele)=>ele==this.currentProductId);
+  nextPrd() {
+    let currentIndex = this.productIDs.findIndex(
+      (ele) => ele == this.currentProductId
+    );
     // console.log(currentIndex);
     let nextPrdID;
-    if(currentIndex<this.productIDs.length)
-    {
-      nextPrdID=this.productIDs[currentIndex+1];
-      this.router.navigate(['/products',nextPrdID]);
-
+    if (currentIndex < this.productIDs.length) {
+      nextPrdID = this.productIDs[currentIndex + 1];
+      this.router.navigate(['/products', nextPrdID]);
     }
-
   }
 }
